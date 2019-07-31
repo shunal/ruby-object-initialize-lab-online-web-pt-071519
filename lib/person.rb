79039c7f99b5621d0eaf813adeb8fb45640e5e1b
Steven Hunal <stevehunal@gmail.com>
2019-07-31 17:27:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -9,3 +9,20 @@ class Person
   end 
   
 end 
+
+class Dog 
+  
+  def initialize(name, breed)
+    @name = name 
+    @breed = breed 
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def breed
+    @breed 
+  end
+  
+end 
